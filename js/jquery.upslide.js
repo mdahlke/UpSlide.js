8b129b02becb3b0f4d08c1c4f06ddf2d89710b4e
Michael Dahlke <madahlke27@gmail.com>
2014-01-17 20:20:17 -0600

    add scroll support for thumbnails

--- js/jquery.upslide.js
+++ js/jquery.upslide.js
@@ -28,16 +28,16 @@
 
 ;(function ($) {
 	
-	function generateColor(t){
+	function generateColor(ht, tt){
 		var r = Math.floor(Math.random() * 255) + 1;
 		var g = Math.floor(Math.random() * 255) + 1;
 		var b = Math.floor(Math.random() * 255) + 1;
-
-		return 'rgba( ' + r + ', ' + g + ', ' + b + ', ' + t + ')';
+		return { 'header': 'rgba( ' + r + ', ' + g + ', ' + b + ', ' + ht + ')', 'indicator': 'rgba( ' + r + ', ' + g + ', ' + b + ', ' + tt + ')' };
 	}
 
 	function init( obj, options ) {
 		var _ = this;
+
 		this.opts = $.extend({
 			speed: 2000,
 			delay: 3000,
@@ -46,129 +46,152 @@
 			fadingHeadersSpeed: 2000,
 			headerHideText: false,
 			headerBackgroundTransparency: 0.5,
-			headerBackgroundColor: ['red', 'green'],
+			headerBackgroundColor: [],
 			headerGenerateBackgroundColor: true,
 			sameSlideAndHeaderColor: true,
 			fullscreen: false,
 			autoSlide: true,
 			arrowControl: false,
 			scrollControl: false,
-			thumbnail: true,
-			thumbnailPosition: 'left',
-			thumbnailHeader: false,
-			thumbnailBorderSize: '0',
-			thumbnailWidth: '10%',
-			thumbnailHeight: 'auto',
-			thumbnailWrapperScrollable: false,
-			thumbnailIndicator: {
+			thumbnail: {
+				show: true,
+				position: 'left',
+				header: false,
+				width: '10%',
+				height: 'auto',
+				scrollable: false
+			},
+			indicator: {
 				speed: 2000,
 				easing: 'linear',
 				backgroundColor: [],
 				width: '2px',
+				transparency: '0.5',
 				position: {
 					side: 'right',
 					inOrOut: 'in'
 				}
 			},
+			ulAlternative: 'ul',
+			liAlternative: 'li',
 			onSlide: function() { return false; },
 			onSlideStart: function() { return false; },
 			onSlideComplete: function() { return false; }
 		}, options);
 		
-		this.calculateSizes = function(){
+		this.objParentID = typeof $(obj).parent().attr('id') === 'undefined' ? Math.round(Math.random(1 * 100)) + 1 : $(obj).parent().attr('id');
+		this.intervalVar = "";
+		this.slideWrapperWidth = 0;
+		this.parentOffsetTop = $(obj).parent().offset().top;
+		this.currentSlide = 0;
+		this.previousSlide = 0;
+		this.numberOfSlides = $(obj).children(this.opts.liAlternative).length;
+		this.slideClass = 'upslide-slide_' + this.objParentID;
+		this.slideNumber = 'data-slideNumber_' + this.objParentID;
+		this.slideOffsetTop = 'data-offsetTop_' + this.objParentID;
+		this.slideLookup = [];
+		this.slides = [];
+		this.backupText = [];
+		this.thumbnailsObject = [];
+		this.thumbnailClass = 'thumb_' + this.objParentID;
+		this.thumbnailSlideNumber = 'data-slideNumberThumb_' + this.objParentID;
+		this.thumbnailWrapper = 'thumbnailWrapper_' + this.objParentID;
+		this.indicator = 'indicator_' + this.objParentID;
+		this.slideThumbnails = [];
+		this.thumbnailHeight = 0;
+		this.thumbnailWidth = 0;
+		this.thumbnailMargin = 0;
+		this.newThumbnailIndicator = "";
+		this.maxTop = 0;
+		this.slideTracker = [];
 		
+		
+		this.calculateSizes = function(){
 			if ( _.opts.fullscreen ){
 				$(obj).parent().width( $(window).width() );
 				$(obj).parent().height( $(window).height() );
 				this.slideHeight = $(window).height();
 				this.slideWidth = $(window).width();			
 				this.slideWrapperWidth = $(window).width();
-				
-				if(this.opts.thumbnail){
-					this.thumbnailWrapperHeight = $(window).height();				
-					this.thumbnailWrapperWidth = ( $(window).width() * (parseFloat(this.opts.thumbnailWidth) / 100) );
-				}
+				this.thumbnailWrapperHeight = $(window).height();				
+				this.thumbnailWrapperWidth = ( $(window).width() * (parseFloat(this.opts.thumbnail.width) / 100) );
+				this.thumbnailBorderWidth = parseInt( $(obj).children().first().css('border-bottom-width') );
 				
 				this.slideHeight = $(window).height();
 				this.slideWidth = this.slideWrapperWidth - this.thumbnailWrapperWidth;
 
-				if( this.opts.thumbnailHeight === 'auto' || !this.thumbnailWrapperScrollable){
-					this.thumbnailHeight = ( ( this.slideHeight / this.numberOfSlides ) - this.opts.thumbnailBorderSize );
-					console.log(this.thumbnailHeight);
-					console.log(this.slideHeight);
-					console.log(this.numberOfSlides);
-					console.log(this.opts.thumbnailBorderSize);
+				if( this.opts.thumbnail.height === 'auto' || !this.opts.thumbnail.scrollable){
+					this.thumbnailHeight = ( ( this.slideHeight / this.numberOfSlides ) - this.thumbnailBorderWidth );
 				}
 				else {
-					this.thumbnailHeight = this.opts.thumbnailHeight;
+					this.thumbnailHeight = this.opts.thumbnail.height;
 				}
 
 			}
 			else {
 				this.slideWrapperWidth = $(obj).parent().width();
-				if(this.opts.thumbnail){
-					this.thumbnailWrapperHeight = $(obj).height();
-					this.thumbnailWrapperWidth = ( $(obj).parent().width() * (parseFloat(this.opts.thumbnailWidth) / 100) );
-				}
+				this.thumbnailWrapperHeight = $(obj).height();
+				this.thumbnailWrapperWidth = ( $(obj).parent().width() * (parseFloat(this.opts.thumbnail.width) / 100) );
 				this.slideHeight = $(obj).height();
 				this.slideWidth = this.slideWrapperWidth - this.thumbnailWrapperWidth;
 				
-				if( this.opts.thumbnailHeight === 'auto' || !this.thumbnailWrapperScrollable){
-					this.thumbnailHeight = ( ( this.slideHeight / this.numberOfSlides )  - this.opts.thumbnailBorderSize  );
+				if( this.opts.thumbnail.height === 'auto' || !this.opts.thumbnail.scrollable){
+					this.thumbnailHeight = ( ( this.slideHeight / this.numberOfSlides )  - this.thumbnailBorderWidth  );
 				}
 				else {
-					this.thumbnailHeight = this.opts.thumbnailHeight;
+					this.thumbnailHeight = this.opts.height;
 				}
 			}
 			
-			if(this.opts.thumbnailPosition === 'right'){
-				this.thumbnailMargin = '0';
-			}
-			else {
-				this.thumbnailMargin = this.thumbnailWrapperWidth;
-			}
+			this.thumbnailMargin = this.opts.thumbnail.position === 'right' ? '0' : this.thumbnailWrapperWidth;
 			
-			if(_.opts.thumbnail){
-				this.thumbnailWidthPX = $(window).width() * parseFloat( '.' + this.opts.thumbnailWidth );
-				this.sliderWidth = parseInt( this.opts.thumbnailWidth );
+			if(_.opts.thumbnail.show){
+				this.thumbnailWidthPX = $(window).width() * parseFloat( '.' + this.opts.thumbnail.width );
+				this.sliderWidth = parseInt( this.opts.thumbnail.width );
 			}
 			
 			this.thumbnailWrapperHeight = this.slideHeight;
 			this.thumbnailWidth = ( ( this.slideWidth / this.numberOfSlides ) );
 		};
 	
+	
+	
 		this.resizePlugin = function(){
 			this.calculateSizes();
 
-			if(this.opts.thumbnail){
+			if(this.opts.thumbnail.show){
 				this.createThumbnailWrapper();
 			}
 
 			this.startTheCreationProcess();
 			this.createSlides();
 
-			if(this.opts.thumbnail){
+			if(this.opts.thumbnail.show){
 				this.createThumbnailAndAddToArray();
-				}
-			if(this.opts.thumbnail){
-				this.createThumbnailIndicator();
+				this.createIndicator();
 			}
 		};
 		
+		
+		
 		this.startTheCreationProcess = function() {
 		
-		if(_.opts.thumbnail){
-			$('.' + this.thumbnailWrapper).empty();
-		}
-		$(obj).css({
-			'margin-left': this.thumbnailMargin + 'px',
-			'width': this.slideWrapperWidth + 'px'
-		});
+			if(_.opts.thumbnail.show){
+				$('.' + this.thumbnailWrapper).empty();
+			}
+			$(obj).css({
+				'top': '0',
+				'margin-left': this.thumbnailMargin + 'px',
+				'width': this.slideWrapperWidth + 'px'
+			});
 
 		};
 			
+			
+			
 		this.createThumbnailWrapper = function(){
-			var overflow = this.opts.thumbnailWrapperScrollable === true ? 'auto' : 'hidden';
+			var overflow = this.opts.thumbnail.scrollable === true ? 'auto' : 'hidden';
+			
 			if( $('.' + this.thumbnailWrapper).length === 0 ){
 				$(obj).parent().prepend($('<div />', {
 						class: 'upslide-thumbnailWrapper ' + this.thumbnailWrapper
@@ -176,11 +199,11 @@
 						'width': this.thumbnailWrapperWidth + 'px', 
 						'height': this.thumbnailWrapperHeight + 'px',
 						'overflow': overflow
-					}).css(_.opts.thumbnailPosition, '0px')
+					}).css(_.opts.thumbnail.position, '0px')
 				);
 			}
 			else {
-				var overflow = this.opts.thumbnailWrapperScrollable === true ? 'auto' : 'hidden';
+				var overflow = this.opts.thumbnail.scrollable === true ? 'auto' : 'hidden';
 				$('.' + this.thumbnailWrapper).css({
 					'width': this.thumbnailWrapperWidth + 'px', 
 					'height': this.thumbnailWrapperHeight + 'px',
@@ -189,61 +212,64 @@
 			}
 		};
 		
-		this.createThumbnailIndicator = function(){
+		
+		
+		this.createIndicator = function(){
 			var bgc, positionValue;
-			if(_.opts.thumbnailIndicator.position.side === 'left') {
-				positionValue = _.opts.thumbnailIndicator.position.inOrOut === 'out' ? '-' + _.opts.thumbnailIndicator.width : '0px';
-				if( typeof this.opts.thumbnailIndicator.backgroundColor === 'object' ){
-					bgc = this.opts.thumbnailIndicator.backgroundColor[_.currentSlide];
+			this.indicatorHeight = parseInt(this.thumbnailHeight) - this.thumbnailBorderWidth;
+			
+			if(_.opts.indicator.position.side === 'left') {
+				positionValue = _.opts.indicator.position.inOrOut === 'out' ? '-' + _.opts.indicator.width : '0px';
+				if( typeof this.opts.indicator.backgroundColor === 'object' ){
+					bgc = this.opts.indicator.backgroundColor[_.currentSlide];
 				}
 				else {
-					bgc = this.opts.thumbnailIndicator.backgroundColor;
+					bgc = this.opts.indicator.backgroundColor;
 				}
-
 				this.newThumbnailIndicator = $('<div />', {
-					class: this.thumbnailIndicator + ' upslide-thumbnailIndicator'
+					class: this.indicator + ' upslide-indicator'
 				}).css({
 					'background-color': bgc,
-					'width': _.opts.thumbnailIndicator.width,
-					'height': this.thumbnailHeight + 'px',
+					'width': _.opts.indicator.width,
+					'height': this.indicatorHeight,
 					'left': positionValue
 				});
 			}
 			else {
-				positionValue = _.opts.thumbnailIndicator.position.inOrOut === 'out' ? '-' + _.opts.thumbnailIndicator.width : '0px';
+				positionValue = _.opts.indicator.position.inOrOut === 'out' ? '-' + _.opts.indicator.width : '0px';
 
-				if( typeof this.opts.thumbnailIndicator.backgroundColor === 'object' ){
-					bgc = this.opts.thumbnailIndicator.backgroundColor[_.currentSlide];
+				if( typeof this.opts.indicator.backgroundColor === 'object' ){
+					bgc = this.opts.indicator.backgroundColor[_.currentSlide];
 				}
 				else {
-					bgc = this.opts.thumbnailIndicator.backgroundColor;
+					bgc = this.opts.indicator.backgroundColor;
 				}
 				this.newThumbnailIndicator = $('<div />', {
-					class: this.thumbnailIndicator + ' upslide-thumbnailIndicator'
+					class: this.indicator + ' upslide-indicator'
 				}).css({
 					'background-color': bgc,
-					'width': _.opts.thumbnailIndicator.width,
-					'height': this.thumbnailHeight + 'px',
+					'width': _.opts.indicator.width,
+					'height': this.indicatorHeight,
 					'right': positionValue
 				});
 			}
+			
 			$('.' + this.thumbnailWrapper).append(this.newThumbnailIndicator);
 		};
+		
+		
 		
 		this.createSlides = function(backupText){
 			this.thumbnailsObject = [];
 			this.slideLookup = [];
 			$('.' + this.thumbnailWrapper).empty();
-			var hci = 0;
-			$(obj).children('li').each(function(i, el){
-				
-				var color = i === 2 ? 'white' : 'white';
-				if( typeof _.opts.headerBackgroundColor[hci] === 'undefined' ){
+			
+			$(obj).children(this.opts.liAlternative).each(function(i, el){
+				if( typeof _.opts.headerBackgroundColor[i] === 'undefined' ){
 					if( _.opts.headerGenerateBackgroundColor ){
-						_.opts.headerBackgroundColor.push( generateColor(_.opts.headerBackgroundTransparency) );
-					}
-					else {
-						hci = 0;
+						var backgroundColors = generateColor(_.opts.headerBackgroundTransparency, _.opts.indicator.transparency);
+						_.opts.headerBackgroundColor.push( backgroundColors.header );
+						_.opts.indicator.backgroundColor.push( backgroundColors.indicator );
 					}
 				}
 
@@ -251,14 +277,13 @@
 					.addClass(_.slideClass)
 					.css({
 						'width': _.slideWidth,
-						'height': _.slideHeight + 'px',
-						'overflow': 'hidden'
+						'height': _.slideHeight + 'px'
 					});
 				_.slideLookup.push( $(el).offset().top - _.parentOffsetTop );
 				
 				_.thumbnailsObject.push({
 					clone: $(this)[0],
-					image: $(this).css('background-image') || $(this).css('background-color'),
+					background: $(this).css('background'),
 					index: i
 				});
 				if(_.opts.headerHideText && backupText) {
@@ -270,24 +295,23 @@
 					_.thumbnailsObject[i].backupText = _.backupText[i];
 				}
 				$(el).children('div').children('.upslide-bannerText').css({
-					'background-color': _.opts.headerBackgroundColor[hci],
-					'color': color
+					'background-color': _.opts.headerBackgroundColor[i]
 				});
 
 				_.slideTracker[i] = 0;
-				hci++;
 			});
 			$('.' + this.thumbnailWrapper).append(this.newThumbnailIndicator);
 			
-			
 		};
+		
+		
 		
 		this.createThumbnailAndAddToArray = function(){
 			for(var i = 0, thumbs = this.thumbnailsObject; i < thumbs.length; i++){
 				var thumb = thumbs[i];
 				var header;
 				
-				if( _.opts.thumbnailHeader && typeof $(thumb.clone).children().children('.upslide-bannerText').text() !== 'undefined'){
+				if( _.opts.thumbnail.header && typeof $(thumb.clone).children().children('.upslide-bannerText').text() !== 'undefined'){
 					header = $(thumb.clone).children().children('.upslide-bannerText').text();
 				}
 				else {
@@ -305,7 +329,7 @@
 				.attr(this.thumbnailSlideNumber, thumb.index)
 				.html( '<span class="upslide-thumbnailTitle textOutline">' + header + '</span>' )
 				.css({
-					'background-image': thumb.image,
+					'background': thumb.background,
 					'background-size': '100% 100%',
 					'background-position': '50%',
 					'float': 'left'
@@ -314,12 +338,19 @@
 			}
 		};
 
+
+
 		this.start = function(){
 			this.interval( 'start' );
 		};
+		
+		
+		
 		this.stop = function(){
 			this.interval( 'stop' );
 		};
+
+
 
 		this.interval = function( action ){
 			if(action === 'start') {
@@ -334,6 +365,8 @@
 
 		};
 		
+		
+		
 		this.fadingHeaders = function( ) {
 			$('.' + this.slideClass).each(function(i, el){
 				if( parseInt( $(el).attr(_.slideNumber) ) !== _.currentSlide && parseInt( $(el).attr(_.slideNumber) ) !== _.previousSlide ) {
@@ -344,6 +377,8 @@
 				}
 			});
 		};
+
+
 
 		this.move = function( dir, animation ) {
 			this.previousSlide = _.currentSlide;
@@ -365,13 +400,15 @@
 				}
 			}
 			
-			if( this.opts.thumbnail ){
+			if( this.opts.thumbnail.show ){
 				this.moveThumbnailToSlide(this.currentSlide);
 			}
 			this.moveToSlide(this.currentSlide, animation);
 
 			return this;
 		};
+
+
 
 		this.moveToSlide = function( number, animation ){
 			var pos = _.slideLookup[number];
@@ -410,14 +447,16 @@
 			});
 		};
 		
+		
+		
 		this.moveThumbnailToSlide = function( slideNumber, animation ){
-			var pos = $('*[' + this.thumbnailSlideNumber + '="' + slideNumber + '"]').offset().top - this.parentOffsetTop;
+			var pos = $('*[' + this.thumbnailSlideNumber + '="' + slideNumber + '"]').offset().top - this.parentOffsetTop + $('*[' + this.thumbnailSlideNumber + '="' + slideNumber + '"]').parent().scrollTop();
 			var bgc;
-			if( typeof this.opts.thumbnailIndicator.backgroundColor === 'object' ){
-				bgc = this.opts.thumbnailIndicator.backgroundColor[slideNumber];
+			if( typeof this.opts.indicator.backgroundColor === 'object' ){
+				bgc = this.opts.indicator.backgroundColor[slideNumber];
 			}
 			else {
-				bgc = this.opts.thumbnailIndicator.backgroundColor;
+				bgc = this.opts.indicator.backgroundColor;
 			}
 			if( typeof bgc === 'undefined' ){
 				bgc = this.opts.headerBackgroundColor[slideNumber];
@@ -428,71 +467,42 @@
 			else {
 				animation = false;
 			}
+			
 			var duration = animation === false ? 0: this.opts.speed;
-		$('.' + this.thumbnailIndicator).stop().animate({
+		$('.' + this.indicator).stop().animate({
 					'top': ( pos - 1 ) + 'px',
 					'background-color': bgc
 				}, {
-					easing: _.opts.thumbnailIndicator.easing,
-					duration: _.opts.thumbnailIndicator.speed,
+					easing: _.opts.indicator.easing,
+					duration: duration,
 					queue: false
 				});
 			
 		};
 		
-		this.objParentID = typeof $(obj).parent().attr('id') === 'undefined' ? Math.round(Math.random(1 * 100)) + 1 : $(obj).parent().attr('id');
-		this.intervalVar = "";
-		this.slideWrapperWidth = 0;
-		this.parentOffsetTop = $(obj).parent().offset().top;
-		this.currentSlide = 0;
-		this.previousSlide = 0;
-		this.numberOfSlides = $(obj).children('li').length;
-		this.slideClass = 'upslide-slide_' + this.objParentID;
-		this.slideNumber = 'data-slideNumber_' + this.objParentID;
-		this.slideOffsetTop = 'data-offsetTop_' + this.objParentID;
-		this.slideLookup = [];
-		this.slides = [];
-		this.backupText = [];
-		this.thumbnailsObject = [];
-		this.thumbnailClass = 'thumb_' + this.objParentID;
-		this.thumbnailSlideNumber = 'data-slideNumberThumb_' + this.objParentID;
-		this.thumbnailWrapper = 'thumbnailWrapper_' + this.objParentID;
-		this.thumbnailIndicator = 'thumbnailIndicator_' + this.objParentID;
-		this.slideThumbnails = [];
-		this.thumbnailHeight = 0;
-		this.thumbnailWidth = 0;
-		this.thumbnailMargin = 0;
-		this.newThumbnailIndicator = "";
-		this.maxTop = 0;
-		this.slideTracker = [];
-		
-		console.log($(obj).parent().offset());
+
+
 		if( this.opts.sameSlideAndHeaderColor ) {
 			if( this.opts.headerBackgroundColor.length !== 0 ) {
-				this.opts.thumbnailIndicator.backgroundColor = this.opts.headerBackgroundColor;
+				this.opts.indicator.backgroundColor = this.opts.headerBackgroundColor;
 			}
-			else if( _.opts.headerBackgroundColor.length === 0 && this.opts.thumbnailIndicator.backgroundColor.length !== 0) {
-				this.opts.headerBackgroundColor = this.opts.thumbnailIndicator.backgroundColor;
-			}
-			else {
-				this.opts.headerBackgroundColor = [];
+			else if( _.opts.headerBackgroundColor.length === 0 && this.opts.indicator.backgroundColor.length !== 0) {
+				this.opts.headerBackgroundColor = this.opts.indicator.backgroundColor;
 			}
 		}
 		
 		this.calculateSizes();
 		
-		if(this.opts.thumbnail){
+		if(this.opts.thumbnail.show){
 			this.createThumbnailWrapper();
 		}
 		
 		this.startTheCreationProcess();
 		this.createSlides(true);
 		
-		if(this.opts.thumbnail){
+		if(this.opts.thumbnail.show){
 			this.createThumbnailAndAddToArray();
-			}
-		if(this.opts.thumbnail){
-			this.createThumbnailIndicator();
+			this.createIndicator();
 		}
 
 
@@ -504,6 +514,8 @@
 		if( this.slideHeight === 0 ){
 			alert('If you don\'t want the plugin to be fullscreen make sure you set a height for the parent of the <ul> it in your css!\n\n The current height for the slides is: ' + this.slideHeight + 'px.');
 		}
+		
+		
 		
 		/***********************************************************************
 		 * 
@@ -532,6 +544,8 @@
 			});
 		}
 		
+		
+		
 		if( this.opts.scrollControl ){
 			
 			$('.' + this.thumbnailClass).mousewheel(function(e,d){
@@ -541,10 +555,14 @@
 			});
 		}
 
+
+
 		$(document).on('click', '.' + this.thumbnailClass, function(e){
 			e.preventDefault();
 			_.move( parseInt( $(this).attr(_.thumbnailSlideNumber) ) );
 		});
+
+
 
 		$(window).resize(function(){
 			$(obj).css({
@@ -554,6 +572,8 @@
 			_.move(_.currentSlide, false);
 		});
 
+
+
 		if( this.opts.autoSlide ){
 			this.start();
 		}
@@ -562,7 +582,7 @@
 	}
 	
 	$.fn.upslide = function( options ){
-		 return new init( this, options );
+		 return  new init( this, options );
 	};
 
 })( jQuery );
